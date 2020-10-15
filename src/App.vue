7289e3d04d4a0c706e8b44dc66ad8ab43c20760f
Hadrien CAZES <Hadrien.Cazes@gmail.com>
2020-10-15 17:54:08 +0200

    bitcoin trend working with chart.js

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2 class="title is-5">Bitcoin Price Trends</h2>
+    <Bitcoin v-if="loaded" :chartdata="chartdata" :options="options"></Bitcoin>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Bitcoin from './components/Bitcoin';
+import axios from 'axios';
+import moment from 'moment';
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
-  }
+    Bitcoin
+  },
+  data: () => ({
+    labels: [],
+    data: [],
+    chartdata: null,
+    options: null,
+    loaded: false
+  }),
+  async mounted () {
+  this.loaded = false;
+  axios.get('http://localhost:3000/data')
+  .then((res) => {
+    for (var i in res.data) {
+      this.labels.push(moment(Date(res.data[i].Date)).format('MMMM Do YYYY, h:mm:ss a'));
+      this.data.push(res.data[i].Weighted_Price);
+    }
+
+    this.chartdata = {
+      labels : this.labels,
+      datasets: [
+        {
+          label: 'Bitcoin price',
+          data: this.data
+        }
+      ]
+    }
+
+
+    this.options = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+
+    this.loaded = true
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
 }
 </script>
 
